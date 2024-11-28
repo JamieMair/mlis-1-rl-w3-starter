2969bf039dec4bd091b1ff9f575c329b26391abf
Jamie Mair <JamieMair@users.noreply.github.com>
2024-11-28 09:55:14 +0000

    Implemented more of the RL problem

--- main.py
+++ main.py
@@ -19,25 +19,40 @@ class Trajectory:
 
         self.discount = discount
 
+    def calculate_partial_returns(self):
+        """Using the stored rewards and discount, calculate the partial returns
+            G_t = \sum_{t'>=t} R_t
+        Returns:
+            np.array: Same shape as 'self.rewards', with G[t] = R[t]+R[t+1]+...R[T], where T=self.rewards.size
+        """
+        partial_returns = np.zeros(self.rewards.shape)
+        #### TODO: Modify 'partial_returns' array so that the i^th index represents the return from time 'i' onwards (i.e. R[i]+R[i+1]+...)
+        # TIP: Work backwards from the final time, using the fact that G[t]=R[t]+G[t+1] and G[T] = 0
+        # partial_returns = ?
+        #### END
+
+        return partial_returns
 class Problem:
     def __init__(self, seed=42, W=5, min_reward=-5, max_reward=-1):
-        #### TODO: Seed numpy's random number generator here using the seed provided
-        # ?
-        #### END
+        # Keep track of the original pseudo-random number generator state to reset to later
+        original_state = np.random.get_state()
+        np.random.seed(seed)
 
         # W is the width of the grid world problem
         self.W = W
         # Starting state is the middle tile (// performs a floor integer divide)
         self.starting_state = int(W // 2)
 
-        #### TODO: Change the goal state to be randomly either 0 or (W-1)
-        self.goal_state = 0
-        #### END
+        # 50/50 chance of the goal being in x=0 or x=W-1
+        self.goal_state = 0 if np.random.random() < 0.5 else self.W - 1
 
         # Randomly initialise some weights
         self.tile_rewards = np.random.rand(W) * (max_reward - min_reward) + min_reward
         # Init an array to store the "theta" values
         self.policy_parameters = np.zeros(W)
+
+        # Reset the random number generator
+        np.random.set_state(original_state)
 
     def run(self):
         x = self.starting_state
@@ -51,15 +66,8 @@ class Problem:
             #### END
 
             # Select a=+1 with probability 'p_right' and a=-1 with probability '1-p_right'
-            a = int((np.random.rand() < 0.5) * 2 - 1)
+            a = int((np.random.rand() < p_right) * 2 - 1)
             ####
-
-            #### TODO: Remove the following code block once probabilities are properly calculated
-            if (x==0 and a == -1):
-                a = 1
-            elif (x==self.W - 1 and a== +1):
-                a = -1
-            #### END
 
             # Step to the next state by applying environment function
             x = x + a
@@ -78,18 +86,31 @@ class Problem:
     def reinforce_gradient_estimate(self, N):
         grads = np.zeros_like(self.policy_parameters)
         avg_return = 0
-        
+
         for _ in range(N):
             trajectory = self.run()
             avg_return += np.sum(trajectory.rewards)
+
+            # Use the method in the trajectory to calculate the partial returns (G)
+            G_t = trajectory.calculate_partial_returns()
+
             #### TODO: Add in the gradient contribution here for the current trajectory
-            # grads += ?
+            # TIP: Gradient contribution is G[t]*∇log π(A[t]|S[t])
+            S_t = trajectory.states[:-1]  # Don't include the terminal state
+            A_t = trajectory.actions
+            theta_t = self.policy_parameters[S_t]
+            # grad_log_prob = ?
+            # Add the contribution to the gradient w.r.t the parameters for each state.
+            # TODO: Uncomment the next three lines once grad_log_probs is calculated
+            # grad_contribution = G_t * grad_log_prob
+            # for x, grad in zip(S_t, grad_contribution):
+            #    grads[x] += grad
             #### END
-        
+
         # Divide by N to find means
         grads = grads / N
         avg_return = avg_return / N
-        
+
         return grads, avg_return
 
     def train(self, epochs: int, N: int, learning_rate: float, use_progress: bool = True):
@@ -112,6 +133,9 @@ class Problem:
 
 
 if __name__ == "__main__":
+    # Seed the random number generator to get the same graph every time
+    np.random.seed(1234)
+
     problem = Problem()
 
     epochs = 5000
